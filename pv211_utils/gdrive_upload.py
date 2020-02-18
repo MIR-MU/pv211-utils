6ab68ceb1e09aa68d0233ed9e94b185b894c3026
Michal Štefánik <michal.stefanik@gaussalgo.com>
2020-02-18 19:55:23 +0100

    init v0.1

---
+++ pv211_utils/gdrive_upload.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+# from google.colab import drive
+import pkg_resources
+import datetime
+
+
+def _get_week(current_date, competition_end=datetime.date(2020, 5, 8)):
+    competition_start_w = datetime.date(2020, 2, 17).isocalendar()[1]
+    competition_end_w = competition_end.isocalendar()[1]
+    if current_date.date() > competition_end:
+        raise ValueError("Sorry, the competition has ended in %s. No more submissions."
+                         % competition_end.strftime("%d.%m.%Y %H:%M:%S"))
+    return current_date.isocalendar()[1] - competition_start_w + 1
+
+
+def log_precision_entry(competitor_name: str, precision: float = 0):
+    if precision > 1 or precision < 0:
+        raise ValueError("That precision (%s) looks suspicious. Is it real?" % precision)
+
+    key_path = pkg_resources.resource_filename("pv211_utils", "data/pv211-leaderboard-b8e892e3e8bb.json")
+
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path, scope)
+    gc = gspread.authorize(credentials)
+
+    now = datetime.datetime.now()
+    current_time = now.strftime("%d.%m.%Y %H:%M:%S")
+
+    spreadsheet_key = '1f9P3bn17n2rHGCxBnn3GVr57PF5hMWJEILp06Uq7Jnk'
+    spreadsheet = gc.open_by_key(spreadsheet_key)
+    logs_worksheet = spreadsheet.worksheet("submissions")
+    scores_worksheet = spreadsheet.worksheet("leaderboard")
+    if competitor_name not in scores_worksheet.col_values(2):
+        raise ValueError("We do not have anyone named '%s' in the leaderboard. Is the spelling correct? "
+                         "\nWe expect the name in format '<Surname>, <Name>', like 'Novotný, Vít'"
+                         % competitor_name)
+
+    logs_list = logs_worksheet.get_all_values()
+    current_len = len(logs_list)
+    current_week = "Week %s" % _get_week(now)
+    # append entry
+    logs_worksheet.append_row([current_len, current_time, current_week, competitor_name, float(precision)])
+    return True
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pv211_utils",
+    version='0.1',
+    description="Utilities for PV211 project",
+    long_description="",
+    classifiers=[],
+    author="Michal Stefanik",
+    author_email="stefanik.m@fi.muni.cz",
+    url="https://gitlab.fi.muni.cz",
+    license="MIT",
+    packages=["pv211_utils"],
+    package_dir={"pv211_utils": "pv211_utils"},
+    use_scm_version={"write_to": ".version", "write_to_template": "{version}\n"},
+    setup_requires=["setuptools_scm"],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        "setuptools",
+        "gspread",
+        "oauth2client",
+        "google",
+    ],
+    package_data={"pv211_utils": ["data/*"]},
+)
+
+# vim: set cin et ts=4 sw=4 ft=python :11
