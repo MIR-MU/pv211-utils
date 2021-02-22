865ae0d17faf9ec777d66f6f708ccdeae97ad846
Vít Novotný <witiko@mail.muni.cz>
2021-02-22 19:51:18 +0100

    Unit-test eval.py

--- test/eval.py
+++ test/eval.py
@@ -1,14 +1,42 @@
+from collections import OrderedDict
 import unittest
+from typing import List, Callable
 
 from pv211_utils.entities import QueryBase, DocumentBase
-from pv211_utils.eval import average_precision
+from pv211_utils.irsystem import IRSystemBase
+from pv211_utils.leaderboard import LeaderboardBase
+from pv211_utils.eval import average_precision, mean_average_precision
 
+
+AUTHOR_NAME = 'John Doe'
 
 QUERY_ID = 123
 FIRST_RELEVANT_DOCUMENT_ID = 1
 SECOND_RELEVANT_DOCUMENT_ID = 2
 FIRST_IRRELEVANT_DOCUMENT_ID = 3
 SECOND_IRRELEVANT_DOCUMENT_ID = 4
+
+
+class GoodIRSystem(IRSystemBase):
+    def search(self, query: QueryBase) -> List[DocumentBase]:
+        first_relevant_document = DocumentBase(FIRST_RELEVANT_DOCUMENT_ID, None)
+        second_relevant_document = DocumentBase(SECOND_RELEVANT_DOCUMENT_ID, None)
+        return [first_relevant_document, second_relevant_document]
+
+
+class BadIRSystem(IRSystemBase):
+    def search(self, query: QueryBase) -> List[DocumentBase]:
+        first_irrelevant_document = DocumentBase(FIRST_IRRELEVANT_DOCUMENT_ID, None)
+        second_irrelevant_document = DocumentBase(SECOND_IRRELEVANT_DOCUMENT_ID, None)
+        return [first_irrelevant_document, second_irrelevant_document]
+
+
+class Leaderboard(LeaderboardBase):
+    def __init__(self, callback: Callable[[str, float], None]):
+        self.callback = callback
+
+    def log_precision_entry(self, author_name: str, mean_average_precision: float) -> None:
+        self.callback(author_name, mean_average_precision)
 
 
 class TestAveragePrecision(unittest.TestCase):
@@ -52,3 +80,58 @@ class TestAveragePrecision(unittest.TestCase):
         ]
         precision = average_precision(self.query, results, self.relevant, self.num_relevant)
         self.assertEqual(1.0, precision)
+
+
+class TestMeanAveragePrecision(unittest.TestCase):
+    def setUp(self):
+        query = QueryBase(QUERY_ID, None)
+
+        self.queries = OrderedDict()
+        self.queries[query.query_id] = query
+
+        first_relevant_document = DocumentBase(FIRST_RELEVANT_DOCUMENT_ID, None)
+        second_relevant_document = DocumentBase(SECOND_RELEVANT_DOCUMENT_ID, None)
+        first_irrelevant_document = DocumentBase(FIRST_IRRELEVANT_DOCUMENT_ID, None)
+        second_irrelevant_document = DocumentBase(SECOND_IRRELEVANT_DOCUMENT_ID, None)
+
+        self.documents = OrderedDict()
+        for document in (
+                    first_relevant_document,
+                    second_relevant_document,
+                    first_irrelevant_document,
+                    second_irrelevant_document,
+                ):
+            self.documents[document.document_id] = document
+
+        self.judgements = set([
+            (query, first_relevant_document),
+            (query, second_relevant_document),
+        ])
+
+        self.good_irsystem = GoodIRSystem()
+        self.bad_irsystem = BadIRSystem()
+
+    def test_mean_average_precision_zero(self):
+        precision = mean_average_precision(self.bad_irsystem, self.queries, self.documents, self.judgements)
+        self.assertEqual(0.0, precision)
+
+    def test_mean_average_precision_one(self):
+        precision = mean_average_precision(self.good_irsystem, self.queries, self.documents, self.judgements)
+        self.assertEqual(1.0, precision)
+
+    def test_mean_average_precision_logging(self):
+        logged_author_name = None
+        logged_precision = None
+
+        def callback(author_name: str, precision: float):
+            nonlocal logged_author_name
+            nonlocal logged_precision
+            logged_author_name = author_name
+            logged_precision = precision
+
+        leaderboard = Leaderboard(callback)
+        mean_average_precision(self.good_irsystem, self.queries, self.documents, self.judgements,
+                               leaderboard, True, AUTHOR_NAME)
+
+        self.assertEqual(AUTHOR_NAME, logged_author_name)
+        self.assertEqual(1.0, logged_precision)
