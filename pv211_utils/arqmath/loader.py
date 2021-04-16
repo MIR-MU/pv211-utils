ee268e5c15410128766bf6fc79d9599337e995f6
Vít Novotný <witiko@mail.muni.cz>
2021-04-16 03:25:52 +0200

    Add query and judgement splits to pv211_utils.arqmath.loader

--- pv211_utils/arqmath/loader.py
+++ pv211_utils/arqmath/loader.py
@@ -21,6 +21,21 @@ TEXT_FORMATS = (
     'xhtml+pmml',
 )
 
+QUERY_SUBSETS = {
+    'train': set([
+        1, 3, 4, 5, 7, 9, 10, 11, 13, 14, 15, 17, 19, 20, 21, 23, 26, 28, 30,
+        33, 35, 36, 37, 38, 39, 41, 43, 45, 47, 50, 52, 55, 56, 58, 59, 60, 61,
+        62, 63, 65, 66, 68, 69, 72, 75, 77, 79, 83, 86, 87, 88, 89, 90, 93,
+        98,
+    ]),
+    'validation': set([
+        12, 18, 24, 32, 40, 48, 54, 67, 74, 85, 96
+    ]),
+    'test': set([
+        8, 16, 27, 29, 42, 44, 49, 51, 53, 80, 99
+    ]),
+}
+
 
 def _check_text_format(text_format: str) -> None:
     if text_format in TEXT_FORMATS:
@@ -45,7 +60,8 @@ def _resolve_query_id(raw_query_id: str) -> int:
     return query_id
 
 
-def load_queries(text_format: str, query_class=ArqmathQueryBase) -> OrderedDict:
+def load_queries(text_format: str, query_class=ArqmathQueryBase,
+                 subset: Optional[str] = None) -> OrderedDict:
     _check_text_format(text_format)
     queries = OrderedDict()
 
@@ -53,6 +69,8 @@ def load_queries(text_format: str, query_class=ArqmathQueryBase) -> OrderedDict:
     with open(pkg_resources.resource_filename('pv211_utils', filename), 'rt') as f:
         for raw_query in json.load(f):
             query_id = _resolve_query_id(raw_query['query_id'])
+            if subset is not None and query_id not in QUERY_SUBSETS[subset]:
+                continue
             query = query_class(
                 query_id=query_id,
                 title=raw_query['title'],
@@ -118,7 +136,7 @@ def load_answers(text_format: str, answer_class=ArqmathAnswerBase,
     return answers
 
 
-def load_judgements(queries: OrderedDict, answers: OrderedDict,
+def load_judgements(queries: OrderedDict, answers: OrderedDict, subset: Optional[str] = None,
                     filter_document_ids: Optional[Set] = None) -> ArqmathJudgements:
     relevant = set()
 
@@ -127,6 +145,8 @@ def load_judgements(queries: OrderedDict, answers: OrderedDict,
         for raw_query_id, document_id in json.load(f):
             query_id = _resolve_query_id(raw_query_id)
             if filter_document_ids is not None and document_id not in filter_document_ids:
+                continue
+            if subset is not None and query_id not in QUERY_SUBSETS[subset]:
                 continue
             query: ArqmathQueryBase = queries[query_id]
             answer: ArqmathAnswerBase = answers[document_id]
