e742418de0f4d373c1756153937283037c1b6778
Vít Novotný <witiko@mail.muni.cz>
2022-02-22 22:34:12 +0100

    Remove undefined variable in download_datasets.py

--- scripts/download_datasets.py
+++ scripts/download_datasets.py
@@ -6,4 +6,4 @@ from pv211_utils.trec.loader import load_documents as trec_load_documents
 if __name__ == '__main__':
     root_directory = Path('/var') / 'tmp' / 'pv211'
     root_directory.mkdir(parents=True, exist_ok=True)
-    trec_load_documents(Document, cache_download=str(root_directory/'trec_documents.json.gz'))
+    trec_load_documents(cache_download=str(root_directory/'trec_documents.json.gz'))
