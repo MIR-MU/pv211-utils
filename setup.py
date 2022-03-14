4e1026b374d589bbdb1bfe3b7711f6df592ff05c
Vít Novotný <witiko@mail.muni.cz>
2022-03-14 20:48:02 +0100

    Increment patch version of pv211-utils

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="pv211_utils",
-    version='1.1.1',
+    version='1.1.2',
     description="Utilities for PV211 project",
     long_description="",
     classifiers=[],
