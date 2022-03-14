b612e0f90030df2856affe42333633bdabde8e00
Vít Novotný <witiko@mail.muni.cz>
2022-03-14 19:36:55 +0100

    Only download text collection for ARQMath in scripts.download_datasets

--- script/download_datasets.py
+++ script/download_datasets.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
 
+BASE_TEXT_FORMATS = ['text']
+
+
 def download_trec(root_directory: Path) -> None:
     from pv211_utils.trec.loader import load_documents
 
@@ -11,9 +14,10 @@ def download_trec(root_directory: Path) -> None:
 def download_arqmath(root_directory: Path) -> None:
     from pv211_utils.arqmath.loader import load_answers, TEXT_FORMATS
 
-    for text_format in TEXT_FORMATS:
-        filename = str(root_directory/f'arqmath2020_answers_{text_format}.json.gz')
-        load_answers(text_format, cache_download=filename)
+    text_formats = set(TEXT_FORMATS) & set(BASE_TEXT_FORMATS)
+    for text_format in text_formats:
+        pathname = str(root_directory/f'arqmath2020_answers_{text_format}.json.gz')
+        load_answers(text_format, cache_download=pathname)
 
 
 def main() -> None:
