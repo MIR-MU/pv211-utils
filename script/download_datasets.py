36be8296da0203bfcddb8af751183fff82cd941b
Vít Novotný <witiko@mail.muni.cz>
2022-03-14 14:13:48 +0100

    Shorten overlong line

--- script/download_datasets.py
+++ script/download_datasets.py
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     root_directory.mkdir(parents=True, exist_ok=True)
     trec_load_documents(cache_download=str(root_directory/'trec_documents.json.gz'))
     for text_format in TEXT_FORMATS:
-        arqmath_load_answers(text_format, cache_download=str(root_directory/f'arqmath2020_answers_{text_format}.json.gz'))
+        arqmath_filename = str(root_directory/f'arqmath2020_answers_{text_format}.json.gz'
+        arqmath_load_answers(text_format, cache_download=))
