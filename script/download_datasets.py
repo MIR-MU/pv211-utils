010178d9c47e82c4f605e6ab24b2d99d964e49d7
Vít Novotný <witiko@mail.muni.cz>
2022-03-15 14:54:40 +0100

    Load questions in script.download_datasets

--- script/download_datasets.py
+++ script/download_datasets.py
@@ -16,12 +16,14 @@ def download_trec(root_directory: Path) -> None:
 
 
 def download_arqmath(root_directory: Path) -> None:
-    from pv211_utils.arqmath.loader import load_answers, TEXT_FORMATS
+    from pv211_utils.arqmath.loader import load_questions, load_answers, TEXT_FORMATS
 
     text_formats = set(TEXT_FORMATS) & set(BASE_TEXT_FORMATS)
     for text_format in text_formats:
-        pathname = str(root_directory/f'arqmath2020_answers_{text_format}.json.gz')
-        load_answers(text_format, cache_download=pathname)
+        answers_pathname = str(root_directory/f'arqmath2020_answers_{text_format}.json.gz')
+        answers = load_answers(text_format, cache_download=answers_pathname)
+        questions_pathname = str(root_directory/f'arqmath2020_questions_{text_format}.json.gz')
+        questions = load_questions(text_format, answers, cache_download=questions_pathname)
 
 
 def main() -> None:
