e5ede3aab06d74f960e4998d7d6b29cccccc51ee
Vít Novotný <witiko@mail.muni.cz>
2022-03-15 10:51:04 +0100

    Move ARQMath submission deadline one week back

--- pv211_utils/arqmath/leaderboard.py
+++ pv211_utils/arqmath/leaderboard.py
@@ -10,7 +10,7 @@ class ArqmathLeaderboard(GoogleSpreadsheetLeaderboardBase):
         return datetime.date(2022, 3, 22)
 
     def get_competition_end(self) -> datetime.date:
-        return datetime.date(2022, 5, 2)
+        return datetime.date(2022, 4, 25)
 
     def _get_key_path(self) -> str:
         key_path = pkg_resources.resource_filename('pv211_utils', 'data/pv211-leaderboard-b8e892e3e8bb.json')
