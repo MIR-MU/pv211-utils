6c45cfba6882e5bb8b218470546d7c5bab0951bf
MarekToma <marek.t.toma@gmail.com>
2023-02-26 13:38:05 +0100

    extended beir.loader tests to cover datasets combining and splitting

--- test/beir/test_loader.py
+++ test/beir/test_loader.py
@@ -1,13 +1,26 @@
 import unittest
 
-from pv211_utils.beir.loader import load_queries, load_documents, load_judgements, load_beir_datasets
+from pv211_utils.beir.loader import (load_queries,
+                                     load_documents,
+                                     load_judgements,
+                                     load_beir_datasets,
+                                     combine_beir_datasets,
+                                     split_beir_dataset,
+                                     create_false_judgements)
 from pv211_utils.beir.entities import RawBeirDataset, RawBeirDatasets
 
 
 NUM_QUERIES = 699
 NUM_DOCUMENTS = 22998
 NUM_JUDGEMENTS = 1696
-NUM_COMBINED_DOCUMENTS = 58374
+
+NUM_COMBINED_DOCUMENTS = 457159
+NUM_COMBINED_TEST_QUERIES = 11831
+NUM_COMBINED_TRAIN_QUERIES = 657
+NUM_COMBINED_DEV_QUERIES = 657
+NUM_COMBINED_TEST_JUDGEMENTS= 1254
+NUM_COMBINED_TRAIN_JUDGEMENTS = 111
+NUM_COMBINED_DEV_JUDGEMENTS = 81
 
 
 class TestLoadQueries(unittest.TestCase):
@@ -25,7 +38,6 @@ class TestLoadQueries(unittest.TestCase):
     def test_query_body(self):
         self.assertEqual('Android chroot ubuntu - is it possible to get ubuntu to recognise usb devices',
                          self.query.body)
-
 
 class TestLoadDocuments(unittest.TestCase):
     def setUp(self):
@@ -66,3 +78,70 @@ class TestLoadJudgements(unittest.TestCase):
 
     def test_irrelevant_document(self):
         self.assertNotIn((self.query, self.irrelevant_document), self.judgements)
+
+
+class TestCombineAndSplit(unittest.TestCase):
+    def setUp(self):
+        android = RawBeirDataset( "android", test = True)
+        english = RawBeirDataset("english", test = True)
+        gaming = RawBeirDataset("gaming", test = True)
+        gis = RawBeirDataset("gis", test = True)
+        mathematica = RawBeirDataset("mathematica", test = True)
+        physics = RawBeirDataset("physics", test = True)
+        programmers = RawBeirDataset("programmers", test = True)
+        stats = RawBeirDataset("stats", test = True)
+        tex = RawBeirDataset("tex", test = True)
+        unix = RawBeirDataset("unix",test = True)
+        webmasters = RawBeirDataset("webmasters", test = True)
+        wordpress = RawBeirDataset("wordpress", test = True)
+        download_location = "datasets"
+
+        # Test loading and combining multiple datasets
+        desired_datasets = RawBeirDatasets(datasets=[android,english,gaming,gis,mathematica,physics,programmers,stats,tex,unix,webmasters,wordpress],
+                                           download_location=download_location)
+        _, _, raw_test_data = load_beir_datasets(desired_datasets)
+
+        # Test splitting the dataset
+        # Leave 90% for training and 10% for validation and testing
+        raw_train_data, raw_test_data = split_beir_dataset(raw_test_data, split_factor=0.9)
+        # Subsequently split this into 5% for validation and 5% for testing
+        raw_dev_data, raw_train_data = split_beir_dataset(raw_train_data, split_factor=0.5)
+
+        raw_corpus_test = list(raw_test_data)[0]
+        raw_queries_test = list(raw_test_data)[1]
+        raw_qrels_test = list(raw_test_data)[2]
+
+        raw_queries_train = list(raw_train_data)[1]
+        raw_qrels_train = list(raw_train_data)[2]
+
+        raw_queries_dev = list(raw_dev_data)[1]
+        raw_qrels_dev = list(raw_dev_data)[2]
+
+        self.documents = load_documents(raw_corpus_test)
+        self.test_queries = load_queries(raw_queries_test)
+        self.test_judgements = load_judgements(self.test_queries, self.documents, raw_qrels_test)
+
+        self.train_queries = load_queries(raw_queries_train)
+        self.train_judgements = load_judgements(self.train_queries, self.documents, raw_qrels_train)
+
+        self.dev_queries = load_queries(raw_queries_dev)
+        self.dev_judgements = load_judgements(self.dev_queries, self.documents, raw_qrels_dev)
+
+        self.document = self.documents['51829']
+
+    def test_number_of_documents(self):
+        self.assertEqual(NUM_COMBINED_DOCUMENTS, len(self.documents))
+
+    def test_document_body(self):
+        self.assertTrue(self.document.body.startswith('I want to send files to android tablet with a application from'))
+        self.assertTrue(self.document.body.endswith(' drive? my application that sent files written via Delphi.'))
+    
+    def test_split_size(self):
+        self.assertTrue(NUM_COMBINED_TEST_QUERIES, len(self.test_queries))
+        self.assertTrue(NUM_COMBINED_TRAIN_QUERIES, len(self.train_queries))
+        self.assertTrue(NUM_COMBINED_DEV_QUERIES, len(self.dev_queries))
+
+        self.assertTrue(NUM_COMBINED_TEST_JUDGEMENTS, len(self.test_judgements))
+        self.assertTrue(NUM_COMBINED_TRAIN_JUDGEMENTS, len(self.train_judgements))
+        self.assertTrue(NUM_COMBINED_DEV_JUDGEMENTS, len(self.dev_judgements))
+        
