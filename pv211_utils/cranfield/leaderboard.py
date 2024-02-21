73ea0717e408e874a8c8b817db852f234f838e8c
MarekToma <marek.t.toma@gmail.com>
2024-02-21 12:01:31 +0100

    Spring2024 first project

--- pv211_utils/cranfield/leaderboard.py
+++ pv211_utils/cranfield/leaderboard.py
@@ -7,21 +7,21 @@ from ..leaderboard import GoogleSpreadsheetLeaderboardBase
 
 class CranfieldLeaderboard(GoogleSpreadsheetLeaderboardBase):
     def get_competition_start(self) -> datetime.date:
-        return datetime.date(2023, 2, 13)
+        return datetime.date(2024, 2, 19)
 
     def get_competition_end(self) -> datetime.date:
-        return datetime.date(2023, 3, 13)
+        return datetime.date(2024, 3, 19)
 
     def _get_key_path(self) -> str:
         key_path = pkg_resources.resource_filename('pv211_utils', 'data/pv211-leaderboard-b8e892e3e8bb.json')
         return key_path
 
     def _get_spreadsheet_key(self) -> str:
-        spreadsheet_key = '1kDRTDUCPTOi0crgIO_WqctuvCazTmZ4V_EoVSvb6VQI'
+        spreadsheet_key = '1K_GnmMTjeacPaUmvjO79abrlqOV8JqvnoxoQICulpAw'
         return spreadsheet_key
 
     def get_public_url(self) -> Optional[str]:
         return (
             'https://docs.google.com/spreadsheets/d/e/'
-            '2PACX-1vSXuOTclZfHWYxh2rf7hfMeLvcCuE5UsJu7BzteyunhPw3z4YNZjCovjmMB6SnDdgjGyenOgdochaEq/pubhtml'
+            '2PACX-1vQga_qzgcLp_IcrOt5xZBq4Pq7jjTwmV6JLMRPwkLwG3K3dm2FrcZT-1GhGItFkNyAxwDaOzzFLOIdu/pubhtml'
         )
