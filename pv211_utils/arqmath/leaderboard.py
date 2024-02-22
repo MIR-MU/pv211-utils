cbb04bfa3b663a5653d39477317e8f15bf979722
MarekToma <marek.t.toma@gmail.com>
2024-02-22 08:51:13 +0100

    second term project leaderboard update

--- pv211_utils/arqmath/leaderboard.py
+++ pv211_utils/arqmath/leaderboard.py
@@ -7,22 +7,22 @@ from ..leaderboard import GoogleSpreadsheetLeaderboardBase
 
 class ArqmathLeaderboard(GoogleSpreadsheetLeaderboardBase):
     def get_competition_start(self) -> datetime.date:
-        return datetime.date(2023, 3, 22)
+        return datetime.date(2024, 3, 27)
 
     def get_competition_end(self) -> datetime.date:
-        return datetime.date(2023, 5, 1)
+        return datetime.date(2024, 5, 7)
 
     def _get_key_path(self) -> str:
         key_path = pkg_resources.resource_filename('pv211_utils', 'data/pv211-leaderboard-b8e892e3e8bb.json')
         return key_path
 
     def _get_spreadsheet_key(self) -> str:
-        spreadsheet_key = '1zGUntkeUmE8oFKgM9WdFbN14IPdGJHDbKWXoiec_lmM'
+        spreadsheet_key = '1ohEA6tcmKKKng0Tx787p112fTWDNgF8O5JMvSF-v0ds'
         return spreadsheet_key
 
     def get_public_url(self) -> Optional[str]:
         return (
             'https://docs.google.com/spreadsheets/d/e/2PACX'
-            '-1vT9GibzjkZJxdFNdgr666TLDIHD46HfTYSEPeKUN-ErxRIN2HGjUtKyfvf6Xg3MZ6cW4p9qcSmR'
-            '-4Rk/pubhtml?gid=0&single=true'
+            '-1vQyaxVj3qnJgeSyOnAtUb7VcielyLnw9MZTNC9vTVZm22'
+            'aH4TqV-aIx2TjDZ9fXSeKveUJA8cVB23XR/pubhtml'
         )
