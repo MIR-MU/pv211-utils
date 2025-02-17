111ace2031a6698f77aad731991a1bff13918ebf
stefanik12 <stefanik.mich@gmail.com>
2025-02-17 23:27:31 +0200

    Updated lib version to force upgrade

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name="pv211_utils",
-    version='2.0.0',
+    version='2.0.1',
     description="Utilities for PV211 project",
     long_description="",
     classifiers=[],
