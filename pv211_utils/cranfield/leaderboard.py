79c2ef7e464ea4a8b3b3a2be711cb2d0888675bc
stefanik12 <stefanik.mich@gmail.com>
2025-02-17 22:28:58 +0200

    Cranfield leaderboard for 2025

--- pv211_utils/cranfield/leaderboard.py
+++ pv211_utils/cranfield/leaderboard.py
@@ -7,21 +7,21 @@ from ..leaderboard import GoogleSpreadsheetLeaderboardBase
 
 class CranfieldLeaderboard(GoogleSpreadsheetLeaderboardBase):
     def get_competition_start(self) -> datetime.date:
-        return datetime.date(2024, 2, 19)
+        return datetime.date(2025, 2, 17)
 
     def get_competition_end(self) -> datetime.date:
-        return datetime.date(2024, 3, 19)
+        return datetime.date(2025, 3, 26)
 
     def _get_key_path(self) -> str:
         key_path = pkg_resources.resource_filename('pv211_utils', 'data/pv211-leaderboard-b8e892e3e8bb.json')
         return key_path
 
     def _get_spreadsheet_key(self) -> str:
-        spreadsheet_key = '1K_GnmMTjeacPaUmvjO79abrlqOV8JqvnoxoQICulpAw'
+        spreadsheet_key = '1LGm715BA85FMQDbO4inZu1s1Ey93HLkDCFXUYGKjrpA'
         return spreadsheet_key
 
     def get_public_url(self) -> Optional[str]:
         return (
             'https://docs.google.com/spreadsheets/d/e/'
-            '2PACX-1vQga_qzgcLp_IcrOt5xZBq4Pq7jjTwmV6JLMRPwkLwG3K3dm2FrcZT-1GhGItFkNyAxwDaOzzFLOIdu/pubhtml'
+            '2PACX-1vSLY-jk70GJZSZjJYMKxh6CMBl47KDP6OFjrY_zIMUF9YRwTLl_DSU1mXCrBPiHyUxqav0URYtVP2PK/pubhtml'
         )
