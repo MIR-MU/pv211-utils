981fc5b1b16c7e726575c3d91c77e640c4a4f366
stefanik12 <stefanik.mich@gmail.com>
2025-03-24 22:03:24 +0200

    BEIR/CQAD leaderboard

--- pv211_utils/beir/leaderboard.py
+++ pv211_utils/beir/leaderboard.py
@@ -7,22 +7,22 @@ from ..leaderboard import GoogleSpreadsheetLeaderboardBase
 
 class BeirLeaderboard(GoogleSpreadsheetLeaderboardBase):
     def get_competition_start(self) -> datetime.date:
-        return datetime.date(2024, 3, 27)
+        return datetime.date(2025, 3, 26)
 
     def get_competition_end(self) -> datetime.date:
-        return datetime.date(2024, 5, 7)
+        return datetime.date(2025, 5, 7)
 
     def _get_key_path(self) -> str:
         key_path = pkg_resources.resource_filename('pv211_utils', 'data/pv211-leaderboard-b8e892e3e8bb.json')
         return key_path
 
     def _get_spreadsheet_key(self) -> str:
-        spreadsheet_key = '1z59ROLx-0AS-Wd6ppmOrXKmx7gZG2SM4KdFgfiwoFPI'
+        spreadsheet_key = '1WSSqb42JliidyguSAyk0uzXkX1h5S-273yvms83My0Y'
         return spreadsheet_key
 
     def get_public_url(self) -> Optional[str]:
         return (
             'https://docs.google.com/spreadsheets/d/e/2PACX'
-            '-1vRc5VbtwD2YUdfTlICSlYwcS8ZjNqkHVJrKgQ2pVBNmUi'
-            'Li9lUkTrYde9Mu6fxJTa07LN4VZ2oTs5jZ/pubhtml'
+            '-1vQZXQfKXkVhkzgCE4JptaFIO07db-J49Vgo5ehRmp3kazb'
+            '5uLwLe_ejwQgdHhxGHYdkhb4HgwJxOjwz/pubhtml'
         )
