4a58f58dd01088fa42080f56ee695b64d3cddc1b
stefanik12 <stefanik.mich@gmail.com>
2025-03-24 22:10:40 +0200

    v2.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name="pv211_utils",
-    version='2.0.6',
+    version='2.0.7',
     description="Utilities for PV211 project",
     long_description="",
     classifiers=[],
