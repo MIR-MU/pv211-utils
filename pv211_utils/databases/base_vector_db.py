87876cbda3848307262c75029dccd9149b6e5ccc
Riingbuffer <m.tucek172839@gmail.com>
2025-06-20 15:27:04 +0200

    adjust db types

--- pv211_utils/databases/base_vector_db.py
+++ pv211_utils/databases/base_vector_db.py
@@ -1,19 +1,19 @@
 from abc import ABC, abstractmethod
 from typing import List
 import numpy as np
-from ..entities import DocumentBase
 
 
 class BaseVectorDB(ABC):
     """
     Vector DB interface for storing and searching embeddings.
     """
+
     @abstractmethod
     def add(self, embeddings: np.ndarray):
         """Add documents and their embeddings to the database."""
         pass
 
     @abstractmethod
-    def search(self, query_embedding: np.ndarray, top_k: int = 10) -> List[DocumentBase]:
+    def search(self, query_embedding: np.ndarray, top_k: int) -> List[int]:
         """Return top-k most similar documents for the query embedding."""
         pass
