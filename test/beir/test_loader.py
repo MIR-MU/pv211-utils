f2cf1708449398c6f629110f662b5ddb4dbedc06
Riingbuffer <m.tucek172839@gmail.com>
2025-06-21 13:44:49 +0200

    run only android tests vol 3

--- test/beir/test_loader.py
+++ test/beir/test_loader.py
@@ -3,8 +3,7 @@ import unittest
 from pv211_utils.beir.loader import (load_queries,
                                      load_documents,
                                      load_judgements,
-                                     load_beir_datasets,
-                                     split_beir_dataset)
+                                     load_beir_datasets)
 from pv211_utils.beir.entities import RawBeirDataset, RawBeirDatasets
 
 
@@ -79,63 +78,3 @@ class TestLoadJudgements(unittest.TestCase):
 
     def test_irrelevant_document(self):
         self.assertNotIn((self.query, self.irrelevant_document), self.judgements)
-
-
-class TestCombineAndSplit(unittest.TestCase):
-    def setUp(self):
-        android = RawBeirDataset("android", test=True)
-        download_location = "datasets"
-
-        # Test loading and combining multiple datasets
-        desired_datasets = RawBeirDatasets(datasets=[android],
-                                           download_location=download_location)
-        _, _, raw_test_data = load_beir_datasets(desired_datasets)
-
-        # Test splitting the dataset
-        # Leave 90% for training and 10% for validation and testing
-        raw_train_data, raw_test_data = split_beir_dataset(raw_test_data, split_factor=0.9)
-        # Subsequently split this into 5% for validation and 5% for testing
-        raw_dev_data, raw_train_data = split_beir_dataset(raw_train_data, split_factor=0.5)
-
-        raw_corpus_test = list(raw_test_data)[0]
-        raw_queries_test = list(raw_test_data)[1]
-        raw_qrels_test = list(raw_test_data)[2]
-
-        raw_queries_train = list(raw_train_data)[1]
-        raw_qrels_train = list(raw_train_data)[2]
-
-        raw_queries_dev = list(raw_dev_data)[1]
-        raw_qrels_dev = list(raw_dev_data)[2]
-
-        self.documents = load_documents(raw_corpus_test)
-        self.test_queries = load_queries(raw_queries_test)
-        self.test_judgements = load_judgements(self.test_queries, self.documents, raw_qrels_test)
-
-        self.train_queries = load_queries(raw_queries_train)
-        self.train_judgements = load_judgements(self.train_queries, self.documents, raw_qrels_train)
-
-        self.dev_queries = load_queries(raw_queries_dev)
-        self.dev_judgements = load_judgements(self.dev_queries, self.documents, raw_qrels_dev)
-
-        self.document = self.documents['1']
-
-    def test_number_of_documents(self):
-        print(f"Number of combined documents: {len(self.documents)}")
-        self.assertEqual(NUM_COMBINED_DOCUMENTS, len(self.documents))
-
-    def test_document_body(self):
-        body_begining_str = "I am playing around with my brand new Motorola Defy" \
-                            " and trying to find a way to manage my contacts."
-        self.assertTrue(self.document.body.startswith(body_begining_str))
-        self.assertTrue(self.document.body.endswith("Does anyone have a solution for me ?"))
-
-    def test_split_size(self):
-        print(len(self.test_queries), len(self.train_queries), len(self.dev_queries))
-        print(len(self.test_judgements), len(self.train_judgements), len(self.dev_judgements))
-        self.assertTrue(NUM_COMBINED_TEST_QUERIES, len(self.test_queries))
-        self.assertTrue(NUM_COMBINED_TRAIN_QUERIES, len(self.train_queries))
-        self.assertTrue(NUM_COMBINED_DEV_QUERIES, len(self.dev_queries))
-
-        self.assertTrue(NUM_COMBINED_TEST_JUDGEMENTS, len(self.test_judgements))
-        self.assertTrue(NUM_COMBINED_TRAIN_JUDGEMENTS, len(self.train_judgements))
-        self.assertTrue(NUM_COMBINED_DEV_JUDGEMENTS, len(self.dev_judgements))
