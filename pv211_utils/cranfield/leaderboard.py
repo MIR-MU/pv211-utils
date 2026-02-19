506a0a4f9e5e52f88742e299103de612dfe2c68b
Tereza Vrabcová <xvrabcov@fi.muni.cz>
2026-02-19 11:05:16 +0100

    update cranfield leaderboard

--- pv211_utils/cranfield/leaderboard.py
+++ pv211_utils/cranfield/leaderboard.py
@@ -6,17 +6,17 @@ from ..leaderboard import GoogleSpreadsheetLeaderboardBase
 
 class CranfieldLeaderboard(GoogleSpreadsheetLeaderboardBase):
     def get_competition_start(self) -> datetime.date:
-        return datetime.date(2025, 2, 17)
+        return datetime.date(2026, 2, 19)
 
     def get_competition_end(self) -> datetime.date:
-        return datetime.date(2025, 12, 12)
+        return datetime.date(2026, 3, 20)
 
     def _get_spreadsheet_key(self) -> str:
-        spreadsheet_key = '1LGm715BA85FMQDbO4inZu1s1Ey93HLkDCFXUYGKjrpA'
+        spreadsheet_key = '18L7dZD1GlRvf-RVEXe9WLr5zmI-ItAfehLMMIDf6YyI'
         return spreadsheet_key
 
     def get_public_url(self) -> Optional[str]:
         return (
             'https://docs.google.com/spreadsheets/d/e/'
-            '2PACX-1vSLY-jk70GJZSZjJYMKxh6CMBl47KDP6OFjrY_zIMUF9YRwTLl_DSU1mXCrBPiHyUxqav0URYtVP2PK/pubhtml'
+            '2PACX-1vTC2L20hNdwaeZr1vaTFwfUdGuzxwyccYkWechI_II_PtB74wYA5aMb1kNJDyD_8oTn1gT0Fz6LcZS3/pubhtml'
         )
